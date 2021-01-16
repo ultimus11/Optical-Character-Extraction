ca5ae84a4be22d2cad41547ee48c3f78e927e4bc
sahil <sahil11panindre@gmail.com>
2021-01-16 21:19:32 +0530

    Add files via upload

---
+++ OCR_from_Scratch/part4/pattern_recognition5.py
@@ -0,0 +1,174 @@
+import cv2
+import numpy as np
+import pattern_data
+def get_grid_array(gray_image,grid_height,grid_width):
+	grid_1 = []
+	grid_2 = []
+	grid_3 = []
+	grid_4 = []
+	grid_5 = []
+	grid_6 = []
+	grid_7 = []
+	grid_8 = []
+	grid_9 = []
+	xposition=0
+	yposition=0
+	for y in gray_image:
+		xposition=0
+		gridx1=[]
+		gridx2=[]
+		gridx3=[]
+		gridx4=[]
+		gridx5=[]
+		gridx6=[]
+		gridx7=[]
+		gridx8=[]
+		gridx9=[]
+		for x in y:
+			#here we get every element of grid in one list
+			#first row
+			#first grid
+			if xposition<=grid_width and yposition<=grid_height:
+				#grid_1.append([x])
+				gridx1.append(x)
+			#second grid
+			if xposition<=grid_width*2 and xposition>=grid_width and yposition<=grid_height:
+				gridx2.append(x)
+			#third grid
+			if xposition<=grid_width*3 and xposition>=grid_width*2 and yposition<=grid_height:
+				gridx3.append(x)
+			#second row
+			#4th grid
+			if xposition<=grid_width and yposition<=grid_height*2 and yposition>=grid_height:
+				gridx4.append(x)
+			#5th grid
+			if xposition<=grid_width*2 and xposition>=grid_width and yposition<=grid_height*2 and yposition>=grid_height:
+				gridx5.append(x)
+			#6th grid
+			if xposition<=grid_width*3 and xposition>=grid_width*2 and yposition<=grid_height*2 and yposition>=grid_height:
+				gridx6.append(x)
+			#third row
+			#7th grid
+			if xposition<=grid_width and yposition<=grid_height*3 and yposition>=grid_height*2:
+				gridx7.append(x)
+			#8th grid
+			if xposition<=grid_width*2 and xposition>=grid_width and yposition<=grid_height*3 and yposition>=grid_height*2:
+				gridx8.append(x)
+			#9th grid
+			if xposition<=grid_width*3 and xposition>=grid_width*2 and yposition<=grid_height*3 and yposition>=grid_height*2:
+				gridx9.append(x)
+			xposition+=1
+		if gridx1:
+			grid_1.append(gridx1)
+		if gridx2:
+			grid_2.append(gridx2)
+		if gridx3:
+			grid_3.append(gridx3)
+		if gridx4:
+			grid_4.append(gridx4)
+		if gridx5:
+			grid_5.append(gridx5)
+		if gridx6:
+			grid_6.append(gridx6)
+		if gridx7:
+			grid_7.append(gridx7)
+		if gridx8:
+			grid_8.append(gridx8)
+		if gridx9:
+			grid_9.append(gridx9)
+		yposition+=1
+
+	return grid_1,grid_2,grid_3,grid_4,grid_5,grid_6,grid_7,grid_8,grid_9
+def line_detection(grid):
+	xposition=0
+	yposition=0
+	black_pixels=[]
+	for y in grid:
+		xposition=0
+		for x in y:
+			if x==0:
+				#note that xposition is first element
+				black_pixels.append([xposition,yposition])
+			xposition+=1
+		yposition+=1
+	print(black_pixels)
+	x_counter={}
+	y_counter={}
+	firsttime=0
+	#possible_orientation=0 means its horizontal line and possible_orientation=1 then its vertical line 3 means empty and 2 means cross
+	possible_orientation=0
+	for x in black_pixels:
+		y=x[1]
+		x=x[0]
+		if firsttime==0:
+			x_counter[x]=1
+			y_counter[y]=1
+			firsttime=1
+		elif firsttime!=0:
+			if x in x_counter:
+				x_counter[x]=x_counter[x]+1
+			else:
+				x_counter[x]=1
+			if y in y_counter:
+				y_counter[y]=y_counter[y]+1
+			else:
+				y_counter[y]=1
+		print(x,y)
+	print(x_counter,y_counter)
+	x_length=len(x_counter)
+	y_length=len(y_counter)
+	print(x_length,y_length)
+	if x_length<y_length:
+		possible_orientation=1
+	elif x_length>y_length:
+		possible_orientation=0
+	elif x_length and y_length and x_length==y_length:
+		possible_orientation=2
+	else:
+		possible_orientation=3
+	return possible_orientation,x_counter,y_counter,black_pixels
+def calculate_slopes(possible_orientation,x_counter,y_counter,black_pixels):
+	for key in x_counter:
+		print(key)
+		for coordinates in black_pixels:
+			x=coordinates[0]
+			y=coordinates[1]
+			if x==key:
+				print("no we are not doing this")
+def grid_partition():
+	original_image = cv2.imread("wowo.png")
+	gray=cv2.cvtColor(original_image,cv2.COLOR_BGR2GRAY)
+	print(gray)
+	height, width, = gray.shape
+	#print(height,width)
+	grid_height = height/3
+	grid_width = width/3
+	grid_1,grid_2,grid_3,grid_4,grid_5,grid_6,grid_7,grid_8,grid_9 = get_grid_array(gray,grid_height,grid_width)
+	#print(grid_1,"\n",grid_2,"\n",grid_3,"\n",grid_4,"\n",grid_5,"\n",grid_6,"\n",grid_7,"\n",grid_8,"\n",grid_9)
+	
+	#created a list which containes line data for each node/grid
+	line_data_in_nodes = []
+	possible_orientation,x_counter,y_counter,black_pixels = line_detection(grid_1)
+	line_data_in_nodes.append(possible_orientation)
+	possible_orientation,x_counter,y_counter,black_pixels = line_detection(grid_2)
+	line_data_in_nodes.append(possible_orientation)
+	possible_orientation,x_counter,y_counter,black_pixels = line_detection(grid_3)
+	line_data_in_nodes.append(possible_orientation)
+	possible_orientation,x_counter,y_counter,black_pixels = line_detection(grid_4)
+	line_data_in_nodes.append(possible_orientation)
+	possible_orientation,x_counter,y_counter,black_pixels = line_detection(grid_5)
+	line_data_in_nodes.append(possible_orientation)
+	possible_orientation,x_counter,y_counter,black_pixels = line_detection(grid_6)
+	line_data_in_nodes.append(possible_orientation)
+	possible_orientation,x_counter,y_counter,black_pixels = line_detection(grid_7)
+	line_data_in_nodes.append(possible_orientation)
+	possible_orientation,x_counter,y_counter,black_pixels = line_detection(grid_8)
+	line_data_in_nodes.append(possible_orientation)
+	possible_orientation,x_counter,y_counter,black_pixels = line_detection(grid_9)
+	line_data_in_nodes.append(possible_orientation)
+
+	print(line_data_in_nodes)
+	idx = pattern_data.data(line_data_in_nodes)
+	print(idx)
+	#calculate_slopes(possible_orientation,x_counter,y_counter,black_pixels)
+grid_partition()
---
+++ OCR_from_Scratch/part4/text6.py
@@ -0,0 +1,188 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+img=np.array(cv2.imread("T.png"))
+character_list=[]
+#check position of colour position in image
+def find_position(colour,image):
+	global character_list
+	xposition=0
+	yposition=0
+	for y in image:
+		xposition=0
+		for x in y:
+			if x==0:
+				#print(xposition,yposition)
+				character_list.append([xposition,yposition])
+			xposition+=1
+		yposition+=1
+#check colour at given position in image
+def check_colour(xposition,yposition,image):
+	xposition_1=0
+	yposition_1=0
+	for y in image:
+		xposition_1=0
+		for x in y:
+			if xposition==xposition_1 and yposition==yposition_1:
+				return x
+			xposition_1+=1
+		yposition_1+=1
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+print(gray)
+find_position([0],gray)
+print(character_list)
+length = len(character_list)
+def search_neighbours(CurrentX,CurrentY):
+	global gray
+	stack=[]
+	p0 =[CurrentX,CurrentY]
+	p1 =[CurrentX-1,CurrentY-1]
+	p2 =[CurrentX, CurrentY-1]
+	p3 =[CurrentX+1,CurrentY-1]
+	p4 =[CurrentX-1,CurrentY]
+	p5 =[CurrentX+1,CurrentY]
+	p6 =[CurrentX-1,CurrentY+1]
+	p7 =[CurrentX,CurrentY+1]
+	p8 =[CurrentX+1,CurrentY+1]
+	colour = check_colour(p0[0],p0[1],gray)
+	if colour == 0:
+		stack.insert(0,p0)
+	colour = check_colour(p1[0],p1[1],gray)
+	if colour == 0:
+		stack.insert(0,p1)
+	colour = check_colour(p2[0],p2[1],gray)
+	if colour == 0:
+		stack.insert(0,p2)
+	colour = check_colour(p3[0],p3[1],gray)
+	if colour == 0:
+		stack.insert(0,p3)
+	colour = check_colour(p4[0],p4[1],gray)
+	if colour == 0:
+		stack.insert(0,p4)
+	colour = check_colour(p5[0],p5[1],gray)
+	if colour == 0:
+		stack.insert(0,p5)
+	colour = check_colour(p6[0],p6[1],gray)
+	if colour == 0:
+		stack.insert(0,p6)
+	colour = check_colour(p7[0],p7[1],gray)
+	if colour == 0:
+		stack.insert(0,p7)
+	colour = check_colour(p8[0],p8[1],gray)
+	if colour == 0:
+		stack.insert(0,p8)
+	print(stack)
+	return stack
+def continuity(continuity_check,neighbours):
+	xy=0
+	for lists in neighbours:
+		print(lists)
+		if lists[0]==continuity_check[0] or lists[0]==continuity_check[0]+1 or lists[0]==continuity_check[0]-1:
+			print(lists[0])
+			xy=1
+		if lists[1]==continuity_check[1] or lists[1]==continuity_check[1]+1 or lists[1]==continuity_check[1]-1:
+			print(lists[1])
+		else:
+			xy=0
+		if xy==1:
+			return True
+	return False
+
+stack1=[]
+character_list_duplicate=[]
+continuity_result=True
+def check_neighbours_main(starting_xy,character_list):
+	global stack1
+	global character_list_duplicate
+	#global continuity_result
+	#testlist=character_list.copy()
+	testlist=character_list
+	stack=[]
+	stack.insert(0,starting_xy)
+	#if continuity_result==True:
+	print("checking neighbours for ",starting_xy[0],starting_xy[1])
+	neighbours=search_neighbours(starting_xy[0],starting_xy[1])
+	if neighbours:
+		for element in neighbours:
+			if element in testlist:
+				testlist.remove(element)
+	print(testlist)
+	if testlist:
+		continuity_check=testlist[0]
+		print("printing x continuity",continuity_check[0],continuity_check[1])
+	#append neighbours into stack1
+	if neighbours:
+		for coordinates in neighbours:
+			if coordinates not in stack1:
+				stack1.append(coordinates)
+	if testlist:
+		if continuity_check:
+			continuity_result = continuity(continuity_check,stack1)
+			print(continuity_result)
+			while continuity_result!=True:
+				if testlist:
+					del testlist[0]
+				elif not testlist:
+					break
+				if testlist:
+					continuity_check=testlist[0]
+					print("printing x continuity",continuity_check[0],continuity_check[1])
+					continuity_result = continuity(continuity_check,stack1)
+					print(continuity_result)
+			if testlist:
+				if continuity_result==True:
+					#stack1.append(neighbours)
+					check_neighbours_main(testlist[0],testlist)
+
+print("outside")
+character_list_duplicate=character_list.copy()
+whatever_returned = check_neighbours_main(character_list[0],character_list_duplicate)
+print("wohoooo")
+print("done",stack1)
+xlist=[]
+ylist=[]
+#stretch_near = cv2.resize(np.float32(stack1),(10, 10),interpolation=cv2.INTER_NEAREST)
+#print(stretch_near)
+for data in stack1:
+	print(data)
+	xlist.append(data[0])
+	ylist.append(data[1])
+#plt.plot(xpoints, ypoints)
+minx=min(xlist)
+maxx=max(xlist)
+miny=min(ylist)
+maxy=max(ylist)
+print("printing min max xy",minx,miny)
+#
+#
+#determine size of the image
+#
+#
+Width=(maxx)-(minx)
+hight=(maxy)-(miny)
+
+#shift coordinates towards the origin of image
+stack2=stack1.copy()
+pixel_coordinates=[]
+new_stack=[]
+for data in stack2:
+	pixel_coordinates=[]
+	pixel_coordinates.insert(0,data[1]-(miny-1))
+	pixel_coordinates.insert(0,data[0]-(minx-1))
+	new_stack.append(pixel_coordinates)
+print("printing stack1",stack1,"\nprinting stack2",new_stack)
+# Create a blank 20x20 black image
+image = np.zeros((hight+2, Width+2, 1), np.uint8)
+# Fill image with red color(set each pixel to red)
+image[:] = (255)
+
+#fill pixels in image
+for data in new_stack:
+	image[data[1],data[0]]=(0)
+
+cv2.imwrite("wowo.png",image)
+#cv2.rectangle(img,(minx-1,miny-1),(maxx+1,maxy+1),(0,255,0),1)
+#cv2.imwrite("wowo.png",img)
+plt.plot(xlist, ylist,"ro")
+plt.axis([0, 20, 0, 20])
+plt.show()
